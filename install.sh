b75c782c6560d754b537d06f570c82613f50737d
Great Cat <great.cat@outlook.com>
2022-05-15 10:50:36 +0800

    Fix

--- install.sh
+++ install.sh
@@ -49,4 +49,4 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 
 git clone https://github.com/gpakosz/.tmux.git ~/.tmux
 ln -s -f ~/.tmux/.tmux.conf ~/.tmux.conf
-cp .tmux/.tmux.conf.local ~
+cp ~/.tmux/.tmux.conf.local ~/.tmux.conf.local
