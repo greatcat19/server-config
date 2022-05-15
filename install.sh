10a0fe8c21e5e6ba4a85542f3a3d3f059134a413
greatcat19 <im.great.cat@gmail.com>
2022-05-15 11:10:14 +0800

    Fix

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ git clone https://github.com/greatcat19/server-config.git
 # if zsh does not exist install it
 if ! type "zsh" >/dev/null; then
   if [ "$IS_LINUX" = true ]; then
-    sudo apt-get install zsh curl
+    sudo apt-get install -y zsh curl
   else
     brew install zsh curl
   fi
@@ -32,7 +32,7 @@ fi
 
 if ! type "tmux" >/dev/null; then
   if [ "$IS_LINUX" = true ]; then
-    sudo apt-get install tmux
+    sudo apt-get install -y tmux
   else
     brew install tmux
   fi
@@ -54,9 +54,11 @@ git clone https://github.com/gpakosz/.tmux.git ~/.tmux
 ln -s -f ~/.tmux/.tmux.conf ~/.tmux.conf
 cp ~/.tmux/.tmux.conf.local ~/.tmux.conf.local
 
-if ! type "nvim" >/dev/null; then
+if ! type "nvim" > /dev/null; then
   if [ "$IS_LINUX" = true ]; then
-    sudo apt-get install neovim
+    sudo add-apt-repository ppa:neovim-ppa/unstable -y
+    sudo apt-get update
+    sudo apt-get install -y neovim
   else
     brew install neovim
   fi
@@ -64,8 +66,14 @@ if ! type "nvim" >/dev/null; then
   echo "alias vim='nvim'" >> $HOME/.rc
 fi
 
-if ! type "lvim" >/dev/null; then
-  curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh | bash - -- -y
+if ! type "lvim" > /dev/null; then
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+  nvm install stable
+
+  bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh) -y
   echo "alias lvim=$HOME/.local/bin/lvim" >> $HOME/.rc
 fi
 
